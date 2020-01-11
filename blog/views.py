1e28ea8d7cc17b45e689da523e7fc7eee3194e4b
Ihor <ihor.udod@elpassion.pl>
2020-01-11 21:23:58 +0100

    Posts added to DB and Admin panel

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'IhorUd',
-        'title': 'blog post 1',
-        'content': 'first post content',
-        'date_posted': 'August 27,2018'
-    },
-    {
-        'author': 'JaneDoe',
-        'title': 'Blog post 2',
-        'content': 'second post content',
-        'date_posted': 'August 28,2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context)
